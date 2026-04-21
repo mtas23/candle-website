ae3762e3d125825faf5338f958b9ef160b7fb9b6
Melike Tasli <taslimelikes@gmail.com>
2026-04-21 23:13:29 +0300

    Update baglan.php

--- baglan.php
+++ baglan.php
@@ -5,10 +5,8 @@ $username = "ray48acandlecom_kullanici";
 $password = "042123Kbu*"; 
 
 try {
-    // charset=utf8mb4 ekleyerek emniyet kemerimizi takıyoruz
     $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     
-    // Tezgahtara "Veri alıp verirken kesinlikle Türkçe (UTF-8) kullan" diyoruz
     $db->exec("SET NAMES 'utf8mb4'");
     $db->exec("SET CHARACTER SET utf8mb4");
     
