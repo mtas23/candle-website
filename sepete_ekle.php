e601a25c5e0fff11d5afd1da1cd543d7dd5f5910
Melike Tasli <taslimelikes@gmail.com>
2026-04-21 23:15:26 +0300

    Update sepete_ekle.php

--- sepete_ekle.php
+++ sepete_ekle.php
@@ -1,18 +1,18 @@
 <?php
-session_start(); // Müşterinin yaka kartına bakıyoruz
-require_once 'baglan.php'; // Cebimizdeki anahtarla arka odadaki defteri açıyoruz
+session_start(); 
+require_once 'baglan.php'; 
 
-// Vitrinden (JavaScript'ten) gelen ürün adını ve fiyatı tutuyoruz
+// JavaScript'ten gelen ürün adını ve fiyatı tutma
 $gelen_urun = $_POST['urun_adi'];
 $gelen_fiyat = $_POST['fiyat'];
 $yaka_karti = session_id(); 
 
-// Deftere (Veritabanına) yazdırıyoruz (INSERT işlemi)
+// Veritabanına yazdırma
 $sorgu = $db->prepare("INSERT INTO sepet (session_id, urun_adi, fiyat) VALUES (?, ?, ?)");
 $yazildi_mi = $sorgu->execute([$yaka_karti, $gelen_urun, $gelen_fiyat]);
 
 if($yazildi_mi) {
-    echo "tamam"; // Vitrine 'başardım' diye bağırıyor
+    echo "tamam"; 
 } else {
     echo "hata";
 }
